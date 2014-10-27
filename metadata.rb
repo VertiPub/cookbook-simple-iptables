f6584972f4c380210b3769f1943362c01e0ef5ad
Tucker <tucker@altiscale.com>
2014-10-27 14:31:23 -0700

    Revert "Add name"

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,9 @@
-name             "iptables"
 maintainer       "Dan Crosta"
 maintainer_email "dcrosta@late.am"
 license          "BSD"
 description      "Simple LWRP and recipe for managing iptables rules"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.1"
+version          "0.3.0"
 name             "simple_iptables"
 
 supports "debian", ">= 6.0"
